33a19d72d812cd5926763570bf102b2068e6b3ea
Noel <castillo752x@gmail.com>
2019-11-08 15:33:06 -0700

    linked db to jpa

---
+++ JPA-MLBB/src/main/java/com/skilldistillery/mlbb/entities/Hero.java
@@ -0,0 +1,128 @@
+package com.skilldistillery.mlbb.entities;
+
+import java.sql.Blob;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Hero {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private int health;
+	
+	private int mana;
+	
+	private String backstory;
+	
+	private Blob image;
+
+	public Hero() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Hero [id=" + id + ", name=" + name + ", health=" + health + ", mana=" + mana + ", backstory="
+				+ backstory + ", image=" + image + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getMana() {
+		return mana;
+	}
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+
+	public String getBackstory() {
+		return backstory;
+	}
+
+	public void setBackstory(String backstory) {
+		this.backstory = backstory;
+	}
+
+	public Blob getImage() {
+		return image;
+	}
+
+	public void setImage(Blob image) {
+		this.image = image;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((backstory == null) ? 0 : backstory.hashCode());
+		result = prime * result + health;
+		result = prime * result + id;
+		result = prime * result + mana;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hero other = (Hero) obj;
+		if (backstory == null) {
+			if (other.backstory != null)
+				return false;
+		} else if (!backstory.equals(other.backstory))
+			return false;
+		if (health != other.health)
+			return false;
+		if (id != other.id)
+			return false;
+		if (mana != other.mana)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ JPA-MLBB/src/test/java/com/skilldistillery/mlbb/entities/HeroTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.mlbb.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HeroTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPA-MLBB");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	@DisplayName("Testing Hero Entity Mappings Returns Valid Result")
+	void test1(){
+		Hero hero = em.find(Hero.class, 1);
+		assertNotNull(hero);
+		assertEquals("granger", hero.getName().toLowerCase());
+	}
+
+}
